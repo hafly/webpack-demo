84f4b4967970c78c8b5be1ee07ffce0cdf42139a
hafly <188150920@qq.com>
2019-05-15 08:53:47 +0800

    代码分离

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import {cube} from './math.js';
-
-if (process.env.NODE_ENV !== 'production') {
-    console.log('Looks like we are in development mode!');
-}
+import _ from 'lodash';
 
 function component() {
-    var element = document.createElement('pre');
+    var element = document.createElement('div');
 
-    element.innerHTML = [
-        'Hello webpack!',
-        '5 cubed is equal to ' + cube(5)
-    ].join('\n\n');
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
 }
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        another: './src/another-module.js'
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'Code Splitting'
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    name: "commons",
+                    chunks: "initial",
+                    minChunks: 2
+                }
+            }
+        }
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
